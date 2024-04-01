f37b8fc8e9b908193cd255caa2793ed997449b70
ecam0038 <ecam0038@student.monash.edu>
2024-04-01 17:33:35 +1100

    Testing email signup, password reset

--- quizitude/src/App.jsx
+++ quizitude/src/App.jsx
@@ -4,24 +4,38 @@ import { supabase } from './supabase/supabaseClient'
 import Auth from './pages/Auth'
 import Account from './pages/Account'
 
-function App() {
-  const [session, setSession] = useState(null)
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import AuthRoute from "./components/AuthRoute";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PasswordReset from "./pages/PasswordReset";
+import Register from "./pages/Register";
+import UpdatePassword from "./pages/UpdatePassword";
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
-
+const App = () => {
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
-    </div>
-  )
-}
+    <>
+      <NavBar />
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Routes>
+            <Route element={<AuthRoute />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+            </Route>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/passwordreset" element={<PasswordReset />} />
+            <Route path="/update-password" element={<UpdatePassword />} />
+          </Routes>
+        </div>
+      </Container>
+    </>
+  );
+};
 
-export default App
+export default App;
