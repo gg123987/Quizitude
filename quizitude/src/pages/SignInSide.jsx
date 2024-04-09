bbe9062763a3a047733106822bd36e746c9a9274
ecam0038 <ecam0038@student.monash.edu>
2024-04-09 14:18:26 +1000

    Changed image to public url

--- quizitude/src/pages/SignInSide.jsx
+++ quizitude/src/pages/SignInSide.jsx
@@ -63,7 +63,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: `url(${backgroundImage})`,
+            backgroundImage: `url(https://images.unsplash.com/photo-1501504905252-473c47e087f8?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
             backgroundRepeat: "no-repeat",
             backgroundSize: "cover",
             backgroundPosition: "center",
