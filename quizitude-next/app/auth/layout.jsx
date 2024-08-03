b2db9aaacfc4eb6a92ca7b0311d43cb805cf3e24
ecam0038 <ecam0038@student.monash.edu>
2024-08-03 16:40:34 +1000

    Automatic redirect

---
+++ quizitude-next/app/auth/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthLayout = ({ children }) => {
+    return (
+        <section>
+            {children}
+        </section>
+    );
+};
+
+export default AuthLayout;
