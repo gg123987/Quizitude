08fde9d864ce7654451ab399b3f6c2d835f29dcb
JasonTjin <128770692+JasonTjin@users.noreply.github.com>
2024-09-03 19:14:59 +1000

    Profile Streaks (#12)
    * Streak file and streak test
    
    * Supabase query for current authenticated user's current streak count
    
    * Now ensures an authenticated user is logged in first before fetching streak info
    
    * Date range calendar to highlight the days part of the streak
    
    * Date range calendar default view set to current day, styling changes
    
    * Displays 0 streaks appropriately
    
    * Spacing and styling changes according to figma designs
    
    * Styling edits

--- src/pages/Settings/Profile.jsx
+++ src/pages/Settings/Profile.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import './profile.css'
 import SettingsNavbar from '@/components/features/Profile/SettingsNavbar/SettingsNavbar'
 import GeneralSettings from '@/components/features/Profile/GeneralSettings/GeneralSettings';
+import Streak from '@/components/features/Profile/Streak/Streak';
 
 export default function Account({ session }) {
   const [loading, setLoading] = useState(true)
@@ -73,32 +74,34 @@ export default function Account({ session }) {
 
   return (
     <div className="profile-container">
-
-    <form onSubmit={updateProfile} className="form-widget">
-      <Avatar
-        url={avatar_url}
-        size={150}
-        onUpload={(filePath) => {
-          setAvatarUrl(filePath); // Set the avatar URL when upload is complete
-        }}
-        />
-        <div className="form-fields">
-          <div className='form-field'>
-            <strong><p id="full_name">{user.user_metadata.full_name || ''}</p></strong>
+      <div>
+        <form onSubmit={updateProfile} className="form-widget">
+          <Avatar
+            url={avatar_url}
+            size={150}
+            onUpload={(filePath) => {
+              setAvatarUrl(filePath); // Set the avatar URL when upload is complete
+            }}
+          />
+          <div className="form-fields">
+            <div className='form-field'>
+              <strong><p id="full_name">{user.user_metadata.full_name || ''}</p></strong>
+            </div>
+            <div className='form-field'>
+              <p id="email">{user.email || ''}</p>
+            </div>
           </div>
-          <div className='form-field'>
-            <p id="email">{user.email || ''}</p>
+          <div className='upload-button-container'>
+            <button className="button block primary" type="submit" disabled={loading}>
+              {loading ? 'Loading ...' : 'Edit Profile'}
+            </button>
           </div>
-        </div>
-      <div className='upload-button-container'>
-        <button className="button block primary" type="submit" disabled={loading}>
-          {loading ? 'Loading ...' : 'Edit Profile'}
-        </button>
+        </form>
+        <SettingsNavbar activeTab={activeTab} setActiveTab={setActiveTab} />
       </div>
-    </form>
-    <SettingsNavbar activeTab={activeTab} setActiveTab={setActiveTab} />
-    {activeTab === 'general-settings' && <GeneralSettings />}
-  </div>
+      {activeTab === 'general-settings' && <GeneralSettings />}
+      {activeTab === 'streak' && <Streak />}
+    </div>
   )
 }
 
