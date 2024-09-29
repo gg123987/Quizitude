b8dd5745e0cd4013ef52a754aa99c9a0cb79d60e
ecam0038 <ecam0038@student.monash.edu>
2024-09-29 20:16:17 +1000

    Resolve react pdf dependency issue

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
+import { createRequire } from "node:module";
+import { defineConfig, normalizePath } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const require = createRequire(import.meta.url);
+const pdfjsDistPath = path.dirname(require.resolve("pdfjs-dist/package.json"));
+const cMapsDir = normalizePath(path.join(pdfjsDistPath, "cmaps"));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: cMapsDir,
+          dest: "",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "pdfjs-dist/build/pdf": "pdfjs-dist/build/pdf.mjs",
     },
   },
   test: {
