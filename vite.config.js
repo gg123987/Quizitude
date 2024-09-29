3278925689f9b233ac33ddf0e11631c2ba37bdba
ecam0038 <ecam0038@student.monash.edu>
2024-09-29 20:53:43 +1000

    Updated dependencies for pdf

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,10 @@
 /// <reference types="vitest" />
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
-import { createRequire } from "node:module";
-import { defineConfig, normalizePath } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
-
-const require = createRequire(import.meta.url);
-const pdfjsDistPath = path.dirname(require.resolve("pdfjs-dist/package.json"));
-const cMapsDir = normalizePath(path.join(pdfjsDistPath, "cmaps"));
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: cMapsDir,
-          dest: "",
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -36,6 +20,7 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
+    include: ["pdfjs-dist"],
     esbuildOptions: {
       loader: {
         ".mjs": "js",
